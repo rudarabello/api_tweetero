ca83915361ab6ca2fed2eb949eb3757b4150e898
rudarabello <ruda.rabello.silva@gmail.com>
2023-01-24 16:11:05 -0300

    build: initial commit

---
+++ src/main/java/com/api/apitweetero/ApitweeteroApplication.java
@@ -0,0 +1,13 @@
+package com.api.apitweetero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitweeteroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitweeteroApplication.class, args);
+	}
+
+}
