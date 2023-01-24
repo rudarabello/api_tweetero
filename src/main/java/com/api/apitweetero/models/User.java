7326c184b0a7150e16634d93c704cc72c5f9035d
rudarabello <ruda.rabello.silva@gmail.com>
2023-01-24 16:20:30 -0300

    feat: add user model

---
+++ src/main/java/com/api/apitweetero/models/User.java
@@ -0,0 +1,29 @@
+package com.api.apitweetero.models;
+
+import com.api.apitweetero.dtos.UsersDTO;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    public User(UsersDTO users) {
+        this.avatar = users.avatar();
+        this.username = users.username();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(length = 30, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String avatar;
+
+}
